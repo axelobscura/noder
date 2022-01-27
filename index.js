245441eadd875c1f55e461ec5622706887469cc2
Axel <axosar@gmail.com>
2022-01-27 14:03:09 -0600

    complete todo

--- index.js
+++ index.js
@@ -17,6 +17,39 @@ app.get("/todos", (request, response) => {
   });
 });
 
+app.put("/todos/:id/complete", (request, response) => {
+  const id = request.params.id;
+
+  const findTodoById = (todos, id) => {
+    for(let i = 0; i < todos.length; i++){
+      if(todos[i].id === parseInt(id)){
+        return i;
+      }
+    };
+
+    return -1;
+  }
+
+  fs.readFile('./store/todos.json', 'utf-8', (err, data) => {
+    if(err){
+      return response.status(500).send('Sorry, somethings wrong');
+    }
+
+    let todos = JSON.parse(data)
+    const todoIndex = findTodoById(todos, id);
+
+    if(todoIndex === -1){
+      return response.status(404).send('Sorry not found');
+    }
+
+    todos[todoIndex].complete = true;
+
+    fs.writeFile('./store/todos.json', JSON.stringify(todos), () => {
+      return response.json({'status':'ok'})
+    })
+  })
+});
+
 app.listen(5000, () => {
   console.log("APP running on 5000");
 });
