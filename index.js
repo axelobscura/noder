39b5ccc93306e2edc7a10937c57d93c4c2e40b13
Axel <axosar@gmail.com>
2022-01-27 13:03:44 -0600

    request todos

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const express = require("express");
+const fs = require("fs");
+const app = express();
 
+app.get("/", (request, response) => {
+  response.send("Hello world");
+});
+
+app.get("/todos", (request, response) => {
+  fs.readFile('./store/todos.json', 'utf-8', (err, data) => {
+    if(err){
+      return response.status(500).send('Sorry, somethings wrong');
+    }
+
+    const todos = JSON.parse(data)
+    return response.json({todos: todos})
+  });
+});
+
+app.listen(5000, () => {
+  console.log("APP running on 5000");
+});
